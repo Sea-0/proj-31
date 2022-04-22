2c945cc1edc7f7f4e0ff457cbe41e53503a82620
Sea-0 <96448556+Sea-0@users.noreply.github.com>
2022-04-22 12:02:36 -0300

    Add files via upload

---
+++ particles.js
@@ -0,0 +1,28 @@
+class Particle{
+	constructor(x,y)
+	{
+
+		this.x=x;
+		this.y=y;
+		this.r=12
+		this.body=Bodies.circle(random(40,300), this.y,this.r)
+        this.color=color(random(0,255),random(0,255),random(0,255))
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+			var rubberpos=this.body.position;		
+			push()
+			translate(rubberpos.x, rubberpos.y);
+			rectMode(CENTER)
+			strokeWeight(4);
+			stroke("black");
+			fill("darkblue");
+			//draw the ellipse here to display the rubber ball
+			ellipseMode(RADIUS)
+            ellipse(0,0,this.r,this.r)
+			pop()
+	}
+
+}
